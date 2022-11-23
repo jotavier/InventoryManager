a48bcdb4d3b1ed031a7e35fd323cbbff8ea14faa
Jucemar Vieira Junior <jucemarvlg@gmail.com>
2022-11-22 22:22:24 -0300

    Refactor

--- product.h
+++ product.h
@@ -2,12 +2,13 @@
 #define PRODUCT_H_INCLUDED
 
 #define PRODUCT_TABLE_NAME "products"
+#define MAX_DESCRIPTION_LENGTH 25
 
 typedef struct Product
 {
     unsigned long amount;
     double price;
-    char description[25];
+    char description[MAX_DESCRIPTION_LENGTH];
 } Product;
 
 #endif // PRODUCT_H_INCLUDED
